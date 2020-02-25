0eb8956dce0e3abf7fd9befcdc80866b5da30818
chris43123 <46337972+chris43123@users.noreply.github.com>
2020-02-25 00:42:57 -0600

    Funciona

--- LOTERIA.FX/src/loteria/fx/FormLoteriaController.java
+++ LOTERIA.FX/src/loteria/fx/FormLoteriaController.java
@@ -5,17 +5,27 @@
  */
 package loteria.fx;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import loteria.bl.LotoServices;
 import loteria.bl.Prueba;
 import loteria.bl.Sucursales;
 
@@ -26,20 +36,23 @@ import loteria.bl.Sucursales;
  */
 public class FormLoteriaController implements Initializable {
 
+//    @FXML
+//    private TextField txtCodigo;
+//
+//    @FXML
+//    private TextField txtRTN;
+//
+//    @FXML
+//    private TextField txtNombreSuc;
+//
+//    @FXML
+//    private TextField txtNombreRepe;
+//
+//    @FXML
+//    private TextField txtDireccion;
+//    
     @FXML
-    private TextField txtCodigo;
-
-    @FXML
-    private TextField txtRTN;
-
-    @FXML
-    private TextField txtNombreSuc;
-
-    @FXML
-    private TextField txtNombreRepe;
-
-    @FXML
-    private TextField txtDireccion;
+    private TextField txtBuscar;
     
     @FXML
     private TableView tbSucursales; 
@@ -56,11 +69,18 @@ public class FormLoteriaController implements Initializable {
     @FXML
     private TableColumn colNombreSuc;
     
-    
     @FXML
     private TableColumn colDireccion;
     
+    @FXML
+    private TableColumn colEditar;
+    
+    @FXML
+    private TableColumn colEliminar;
+    
     Sucursales Suc;
+    
+    LotoServices servicio;
     
     ObservableList<Sucursales> dataSucursales;
             
@@ -70,32 +90,115 @@ public class FormLoteriaController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        Suc = new Sucursales();
+        servicio = new LotoServices();
         
-        colCodigo.setCellValueFactory(new PropertyValueFactory("Codigo"));
-        colRTN.setCellValueFactory(new PropertyValueFactory("RTN"));
+        colCodigo.setCellValueFactory(new PropertyValueFactory<>("Codigo"));
+        colRTN.setCellValueFactory(new PropertyValueFactory<>("RTN"));
         colNombreSuc.setCellValueFactory(new PropertyValueFactory("Nombre"));
         //colEncargado.setCellValueFactory(new PropertyValueFactory("Encargado"));
         colDireccion.setCellValueFactory(new PropertyValueFactory("Direccion"));
         colResponsable.setCellValueFactory(new PropertyValueFactory("Responsable"));
-        
         dataSucursales = FXCollections.observableArrayList();
         
-        tbSucursales.setItems(dataSucursales);
+        definirColumnaEditar();
+        definirColumnaEliminar();
+       
+       //cargarDatos();
     }    
     
+//    
+    public void nuevoSucursales() throws IOException {
+        Sucursales nuevoSucursales = new Sucursales();
+        abrirVentanaModal(nuevoSucursales, "Nuevo Sucursales");
+    }
+//    
+    public void buscar() {
+        cargarDatos();
+    }
+//    
+    public String guardar(Sucursales Sucursal) {
+        String resultado = servicio.guardar(Sucursal);
+        if (resultado.equals("")) {
+            cargarDatos();          
+        }
+        return resultado;
+    }
+//    
+    private void cargarDatos() {
+        if (txtBuscar.getText() == null || txtBuscar.getText().equals("")) {
+            dataSucursales = FXCollections.observableArrayList(servicio.obtenerSucursaless());     
+        } else {
+            dataSucursales = FXCollections
+                    .observableArrayList(servicio.obtenerSucursaless(txtBuscar.getText()));
+        }
+              
+       tbSucursales.setItems(dataSucursales);
+       tbSucursales.refresh();
+    }
+//
+    private void abrirVentanaModal(Sucursales Sucursal, String titulo) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("NuevoEditarSucursales.fxml"));
+        Parent root = (Parent) loader.load();
+        
+        NuevoEditarSucursalesController controller = loader.getController();
+        controller.setController(this);
+        controller.setSucursal(Sucursal);
+        
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle(titulo);
+        stage.show();
+    }
     
-    public void RegistrarSuc()
-    {
-        Sucursales NewSucur = new Sucursales();
-        
-        NewSucur.setCodigo(Integer.parseInt((txtCodigo.getText())));
-        NewSucur.setRTN(Integer.parseInt((txtRTN.getText())));
-        NewSucur.setNombre(txtNombreSuc.getText());
-        NewSucur.setResponsable(txtNombreRepe.getText());
-        NewSucur.setDireccion(txtDireccion.getText());
-        NewSucur.setEncargado(txtNombreRepe.getText());
-        
-        dataSucursales.add(NewSucur);
+    private void definirColumnaEditar() {
+        colEditar.setCellFactory(param -> new TableCell<String, String>() {
+            final Button btn = new Button("Editar");
+            
+            @Override
+            public void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                    setText(null);
+                } else {
+                    btn.setOnAction(event -> {
+                        Sucursales Sucursales = (Sucursales) getTableRow().getItem();
+                        try {
+                            abrirVentanaModal(Sucursales, "Editar Sucursales");
+                        } catch (IOException ex) {
+                            Logger.getLogger(NuevoEditarSucursalesController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    });
+                    setGraphic(btn);
+                    setText(null);
+                }
+            }            
+        });
+    }
+
+    private void definirColumnaEliminar() {
+        colEliminar.setCellFactory(param -> new TableCell<String, String>() {
+            final Button btn = new Button("Eliminar");
+            
+            @Override
+            public void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                    setText(null);
+                } else {
+                    btn.setOnAction(event -> {
+                        Sucursales Sucursales = (Sucursales) getTableRow().getItem();
+                        servicio.eliminar(Sucursales);
+                        cargarDatos();
+                    });
+                    setGraphic(btn);
+                    setText(null);
+                }
+            }            
+        });        
     }
 }
--- LOTERIA.FX/src/loteria/fx/NuevoEditarSucursalesController.java
+++ LOTERIA.FX/src/loteria/fx/NuevoEditarSucursalesController.java
@@ -7,7 +7,14 @@ package loteria.fx;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
+import loteria.bl.Sucursales;
 
 /**
  * FXML Controller class
@@ -16,6 +23,26 @@ import javafx.fxml.Initializable;
  */
 public class NuevoEditarSucursalesController implements Initializable {
 
+    @FXML
+    private TextField txtCodigo;
+
+    @FXML
+    private TextField txtRTN;
+
+    @FXML
+    private TextField txtNombreSuc;
+
+    @FXML
+    private TextField txtNombreRepe;
+
+    @FXML
+    private TextField txtDireccion;
+    
+    @FXML
+    Button btnCancelar;
+    
+    private FormLoteriaController controller;
+    private Sucursales Sucursal;
     /**
      * Initializes the controller class.
      */
@@ -23,5 +50,41 @@ public class NuevoEditarSucursalesController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    public void aceptar() {
+        String resultado = controller.guardar(Sucursal);
+        if (resultado.equals("")) {
+            cerrar();   
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Sucursales");
+            alert.setHeaderText("Errores de validación de datos");
+            alert.setContentText(resultado);
+            alert.showAndWait();
+        }
+    }
+    
+    public void cancelar() {
+        cerrar();
+    }
+    
+    private void cerrar() {
+        Stage stage = (Stage) btnCancelar.getScene().getWindow();
+        stage.close();
+    }
+    
+    public void setController(FormLoteriaController controller) {
+        this.controller = controller;
+    }
+    
+    public void setSucursal(Sucursales Sucursal) {
+        this.Sucursal = Sucursal;
+        
+        txtCodigo.textProperty().bindBidirectional(Sucursal.idProperty(), new NumberStringConverter());
+        txtRTN.textProperty().bindBidirectional(Sucursal.RTNProperty(), new NumberStringConverter());        
+        txtNombreSuc.textProperty().bindBidirectional(Sucursal.NombreProperty());        
+        txtDireccion.textProperty().bindBidirectional(Sucursal.DireccionProperty());        
+        txtNombreRepe.textProperty().bindBidirectional(Sucursal.ResponsableProperty());              
+    }
     
 }
